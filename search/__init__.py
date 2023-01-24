e95e99550d2786b99e36d1265de38b706902c29d
Giovanni Aviles <giovanni.aviles@ucsf.edu>
2023-01-23 18:59:02 -0800

    Edited graph.py and created pyproject.toml file

--- search/__init__.py
+++ search/__init__.py
@@ -2,3 +2,11 @@
 BMI203: Biocomputing algorithms Winter 2022
 Assignment 2: Breadth-first search
 """
+import networkx as nx
+import matplotlib.pyplot as plt
+
+import graph
+
+__version__ = "0.1.0"
+__author__ = "Giovanni Aviles"
+
--- search/graph.py
+++ search/graph.py
@@ -1,3 +1,5 @@
+import os
+import queue
 import networkx as nx
 
 class Graph:
@@ -12,7 +14,7 @@ class Graph:
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
 
-    def bfs(self, start, end=None):
+    def bfs(self, start, end):
         """
         TODO: write a method that performs a breadth first traversal and pathfinding on graph G
 
@@ -21,8 +23,45 @@ class Graph:
         * If there is an end node input and a path does not exist, return None
 
         """
+
+        # filename = input("Enter path to adjlist file: ")
+
+        graph = self.graph # write terminal prompt for path to file
+        q = queue.Queue()
+        visited = []
+        q.put(start)
+        visited.append(start)
+        while q:
+            v = q.get()
+            n = [n for n in nx.neighbors(graph, v)]
+            for w in n:
+                if w not in visited:
+                    visited.append(w)
+                    q.put(w)
+        print(q)
+
+
+        """
+        add source node to queue
+        add source neighbors to queue
+        go to first node in queue
+        add its neighbors
+        go to next node in queue
+        add its neighbors to queue
+        check if in queue already, don't add
+        
+        """
+
+        """
+        if end is None:
+            return *list of nodes with order of BFS traversal*
+        else:
+            if path exists:
+                return *list of nodes with order of shortest path*
+            else:
+                return None
+
         return
-
-
+        """
 
 
